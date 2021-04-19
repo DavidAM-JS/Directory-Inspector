73fb717f428da8bfb4bb660422146e7a8f0abba0
David Almaraz <david.almaraz@theksquare.com>
2021-04-18 20:00:44 -0500

    changes2

--- inspector.js
+++ inspector.js
@@ -4,16 +4,22 @@ const path = require('path');
 const directory = (process.argv[2] === undefined) ? process.cwd() : process.argv[2];
 
 (function showFiles(directory) {
-    const files = fs.readdirSync(directory);
-    if(files.length > 0){
-        files.forEach((element) =>{
-        let file = path.join(directory, element)
-            if(fs.statSync(file).isDirectory()){
-                console.log(`${element}/`);
-            }else{
-                console.log(element)
-            }
-        })
-  };
+    try {
+        const files = fs.readdirSync(directory);
+        if (files.length > 0) {
+            files.forEach((element) => {
+                let file = path.join(directory, element)
+                if (fs.statSync(file).isDirectory()) {
+                    console.log(`${element}/`);
+                } else {
+                    console.log(element);
+                }
+            })
+        };
+    } catch (error) {
+        if(error.code === 'ENOENT'){
+          console.log('The directory does not exists');
+        }
+    }
 })(directory);
 
